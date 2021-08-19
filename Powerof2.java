47ee5d26a04fdb8113297bd688840921a3db458d
shraazp <shravyap99@gmail.com>
2021-08-19 14:40:16 +0530

    added

---
+++ Powerof2.java
@@ -0,0 +1,19 @@
+package javapractice;
+
+public class Powerof2 {
+	public static void main(String[] args)
+	{
+	int n=Integer.parseInt(args[0]);
+	if(n>0 && n<31)
+	{
+		for(int i=0;i<=n;i++)
+		{
+			System.out.println("2^"+i+"="+Math.pow(2,i));
+		}
+	}
+	else
+	{
+		System.out.println("Enter number between 0 and 31 !!!");
+	}
+	}
+}
