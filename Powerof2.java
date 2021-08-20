b3309042697bd9ce56feb030570ae5a5a024e351
shravya p <48832081+shraazp@users.noreply.github.com>
2021-08-20 10:54:01 +0530

    added: Program to get power of 2

--- Powerof2.java
+++ Powerof2.java
@@ -3,12 +3,12 @@ package javapractice;
 public class Powerof2 {
 	public static void main(String[] args)
 	{
-	int n=Integer.parseInt(args[0]);
-	if(n>0 && n<31)
+	int n=Integer.parseInt(args[0]);		//@input n is obtained from arguments passed in command line
+	if(n>0 && n<31)					//check if input is between 0 and 31
 	{
 		for(int i=0;i<=n;i++)
 		{
-			System.out.println("2^"+i+"="+Math.pow(2,i));
+			System.out.println("2^"+i+"="+Math.pow(2,i));		//to get power of 2
 		}
 	}
 	else
