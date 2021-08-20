1b76e5561273accc0369ef8fef662a5892b003c4
shravya p <48832081+shraazp@users.noreply.github.com>
2021-08-20 10:43:01 +0530

    added:Program to find prime factors

--- Factors.java
+++ Factors.java
@@ -1,3 +1,4 @@
+//Java program to find prime factors
 package javapractice;
 import java.util.*;
 public class Factors {
@@ -6,14 +7,14 @@ public class Factors {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the number to find prime factor");
-		int n=sc.nextInt();
+		int n=sc.nextInt();						//@input n whose prime factor is tot be calculated
 		System.out.println("Prime factors of "+n+" are");
-		for(int i=2;i<=n;i++)
+		for(int i=2;i<=n;i++)						//@for loop to iterate through numbers between 2 and n
 		{
-			while(n%i==0)
+			while(n%i==0)						//to check if i is factor of n
 			{
-				System.out.print(i+" ");
-				n=n/i;
+				System.out.print(i+" ");			//@output gives the prime factors one by one
+				n=n/i;						//to get prime factors only
 			}
 		}
 		sc.close();
