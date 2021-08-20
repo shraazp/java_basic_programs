11b46d827e059c1a85b4ece49be3bd972add4783
shravya p <48832081+shraazp@users.noreply.github.com>
2021-08-20 10:48:34 +0530

    added: Program to calculate harmonic number

--- HarmoincNumber.java
+++ HarmoincNumber.java
@@ -1,3 +1,4 @@
+//Java program to calculate the harmonc number
 package javapractice;
 import java.util.*;
 public class HarmoincNumber {
@@ -6,11 +7,11 @@ public class HarmoincNumber {
 		// TODO Auto-generated method stub
 		System.out.println("Enter the harmonic number:");
 		Scanner sc=new Scanner(System.in);
-		double n=sc.nextDouble();
+		double n=sc.nextDouble();	//@input
 		double sum=0;
 		for(double i=1;i<=n;i++)
 		{
-			sum=sum+(1/i);
+			sum=sum+(1/i);		//Calculates the harmonic number
 		}
 		System.out.println("The harmonic value is "+sum);
 		sc.close();
