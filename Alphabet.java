003b9b051f5de338532066121bab50526bb0886e
shravya p <48832081+shraazp@users.noreply.github.com>
2021-08-20 10:33:55 +0530

    added:Program to check if character is vowel or consonant

--- Alphabet.java
+++ Alphabet.java
@@ -1,3 +1,4 @@
+//Java program to check if a character is vowel or consonant
 package javapractice;
 
 import java.util.Scanner;
@@ -8,15 +9,15 @@ public class Alphabet {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the alphabet");
-		char ch=sc.next().charAt(0);
-		if(Character.isAlphabetic(ch)) 
+		char ch=sc.next().charAt(0);  		//@input ch to get character from the inut line in console
+		if(Character.isAlphabetic(ch))  	//@function isAlphabetic to check if the character is an alphabet
 		{
-			if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
+			if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')	//@if condition to check if character is vowel
 			{
-				System.out.println(ch+" is a vowel");
+				System.out.println(ch+" is a vowel");							//@print statement prints if character is vowel
 			}
 			else
-				System.out.println(ch+" is a consonant");
+				System.out.println(ch+" is a consonant");						//@print statement prints if a character is consonant
 		}
 		else
 			System.out.println("Enter an alphabet!!!!");
