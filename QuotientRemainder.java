463d696a72c121af60dcb733246ed540dcbad46e
shravya p <48832081+shraazp@users.noreply.github.com>
2021-08-20 10:55:40 +0530

    added: Program to calculate quotient and remainder

--- QuotientRemainder.java
+++ QuotientRemainder.java
@@ -1,3 +1,4 @@
+//Java program to find quotient and remainder
 package javapractice;
 import java.util.*;
 public class QuotientRemainder {
@@ -8,8 +9,8 @@ public class QuotientRemainder {
 		System.out.println("Enter the numbers to find quotient and remainder");
 		int n1=sc.nextInt();
 		int n2=sc.nextInt();
-		System.out.println("Quotient for "+n1+"/"+n2+" is "+n1/n2);
-		System.out.println("Remainder for "+n1+"/"+n2+" is "+n1%n2);
+		System.out.println("Quotient for "+n1+"/"+n2+" is "+n1/n2);	//quotient
+		System.out.println("Remainder for "+n1+"/"+n2+" is "+n1%n2);	//remainder
 		sc.close();
 	}
 
