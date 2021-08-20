dbae771d7c3160fdc4142b688eb5cc6d0d90634f
shravya p <48832081+shraazp@users.noreply.github.com>
2021-08-20 10:50:20 +0530

    added:Program to get largest of three numbers

--- LargestOfThree.java
+++ LargestOfThree.java
@@ -8,12 +8,12 @@ public class LargestOfThree {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the three numbers");
-		int a=sc.nextInt();
+		int a=sc.nextInt();			//@input three numbers from console
 		int b=sc.nextInt();
 		int c=sc.nextInt();
-		if(a>b && a>c)
+		if(a>b && a>c)					//checks if a is greater than b and c
 			System.out.println(a+" is largest");
-		else if(b>a && b>c)
+		else if(b>a && b>c)				//checks if b is greater than a and c
 			System.out.println(b+ " is largest");
 		else
 			System.out.println(c+" is largest");
